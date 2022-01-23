4bef08adbc60f6831a46f5a34172c11bdfa3d150
Rachel Manning <r@rachelmanning.com>
2022-01-22 19:44:05 -0700

    complete part 1 from 7.3.2

---
+++ src/components/GalleryItem.js
@@ -0,0 +1,14 @@
+// GalleryItem.js
+import { useState } from 'react' 
+
+function GalleryItem(props){
+    let [view, setView] = useState(false)
+
+    return (
+        <div onClick={() =>setView(!view)} style={{'display': 'inline-block'}}>
+            <p>One Gallery Item</p>
+        </div>
+    )
+}
+
+export default GalleryItem
