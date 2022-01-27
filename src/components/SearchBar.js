498db68a702c4f724014c122fcce6965abf7850b
Rachel Manning <r@rachelmanning.com>
2022-01-26 23:31:32 -0700

    update searchbar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -9,7 +9,7 @@ function SearchBar(props) {
 
             <input type="text" placeholder="Enter a search term here" onChange={
                 (e) => setSearchTerm(e.target.value)
-            }/>
+            } />
 
             <input type="submit" />
 
