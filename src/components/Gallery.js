6901fffe5a0ce03fcf3529bbea227a080fd74512
Kirby Douglas Ellingson <hkellingson@gmail.com>
2022-04-16 22:03:51 -0500

    fixed API fetch

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -3,10 +3,10 @@ import { useContext } from 'react'
 import { DataContext } from '../context/DataContext'
 import GalleryItem from './GalleryItem'
 
-function Gallery(){
+function Gallery(props){
     const data = useContext(DataContext)
     
-    const display = data.map((item,index) => {
+    const display = props.data.map((item,index) => {
         return (
             <GalleryItem item={item} key={index} />
         )
