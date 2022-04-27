831f658ec1a4069346d205da19f06c3ea6ead5f0
Austin bambino <link9215@gmail.com>
2022-04-26 23:22:05 -0400

    yes

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,17 @@
+
+import { useState } from "react";
+
+function Searchbar(props) {
+    const {term, handleSearch} = useContext(SearchContext)
+
+  return (
+    <form>
+      <input ref={term} type="text" placeholder="Enter a search term here" />
+      <button onClick={(e) => handleSearch(e, term.current.value)}>
+        Submit
+      </button>
+    </form>
+  );
+}
+
+export default Searchbar;
