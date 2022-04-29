3b914e1b8c53833328f249443363ed058cb8acb5
Austin bambino <link9215@gmail.com>
2022-04-28 21:40:25 -0400

    wit suspense

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,43 @@
-import { Fragment, useEffect, useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import { useState, Suspense, useEffect } from "react";
 import Gallery from "./components/Gallery";
 import SearchBar from "./components/SearchBar";
-import AlbumView from "./components/AlbumView";
-import ArtistView from "./components/ArtistView";
+import Spinner from "./components/Spinner";
+import { createResource as fetchData } from "./helper";
 
 function App() {
-  let [search, setSearch] = useState("");
+  let [searchTerm, setSearchTerm] = useState("");
   let [message, setMessage] = useState("Search for Music!");
-  let [data, setData] = useState([]);
-
-  const API_URL = "https://itunes.apple.com/search?term=";
+  let [data, setData] = useState(null);
 
   useEffect(() => {
-    if (search) {
-      const fetchData = async () => {
-        document.title = `${search} Music`;
-        const response = await fetch(API_URL + search);
-        const resData = await response.json();
-        if (resData.results.length > 0) {
-          return setData(resData.results);
-        } else {
-          return setMessage("Not Found");
-        }
-      };
-      fetchData();
+    if (searchTerm) {
+      document.title = `${searchTerm} Music`;
+      console.log(fetchData(searchTerm));
+      setData(fetchData(searchTerm));
     }
-  }, [search]);
+  }, [searchTerm]);
 
   const handleSearch = (e, term) => {
     e.preventDefault();
-    setSearch(term);
+    setSearchTerm(term);
+  };
+
+  const renderGallery = () => {
+    if (data) {
+      return (
+        <Suspense fallback={<Spinner />}>
+          <Gallery data={data} />
+        </Suspense>
+      );
+    }
   };
 
   return (
-    <div>
+    <div className="App">
+      <SearchBar handleSearch={handleSearch} />
       {message}
-      <Router>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <Fragment>
-                <SearchBar handleSearch={handleSearch} />
-                <Gallery data={data} />
-              </Fragment>
-            }
-          />
-          <Route path="/album/:id" element={<AlbumView />} />
-          <Route path="/artist/:id" element={<ArtistView />} />
-        </Routes>
-      </Router>
+      {renderGallery()}
     </div>
   );
 }
