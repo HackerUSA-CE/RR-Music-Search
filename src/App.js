f1a7a84db9dee9e936659f3ff6e47bdb8ee146bc
Neel Patel <neelpatel@Neels-MacBook-Pro.local>
2022-09-15 17:44:25 -0400

    Music Search

--- src/App.js
+++ src/App.js
@@ -1,73 +1,7 @@
-// import { useEffect, useState, useRef } from "react";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Gallery from "./components/Gallery";
-// import SearchBar from "./components/SearchBar";
-// import AlbumView from "./components/AlbumView";
-// import ArtistView from "./components/ArtistView";
-// import { Fragment } from "react/cjs/react.production.min";
-
-// function App() {
-//   let [search, setSearch] = useState("");
-//   let [message, setMessage] = useState("Search for Music!");
-//   let [data, setData] = useState([]);
-//   // let searchInput = useRef("");
-
-//   const API_URL = "https://itunes.apple.com/search?term=";
-
-//   const handleSearch = (e, term) => {
-//     e.preventDefault();
-//     const fetchData = async () => {
-//       document.title = `${term} Music`;
-//       const response = await fetch(API_URL + term);
-//       const resData = await response.json();
-//       if (resData.results.length > 0) {
-//         setData(resData.results);
-//       } else {
-//         setMessage("Not Found");
-//       }
-//     };
-//     fetchData();
-//   };
-
-//   return (
-//     <div>
-//       {message}
-//       <Router>
-//         <Routes>
-//           <Route
-//             path="/"
-//             element={
-//               <Fragment>
-//                 <SearchBar handleSearch={handleSearch} />
-//                 <Gallery data={data} />
-//               </Fragment>
-//             }
-//           />
-//           <Route path="/album/:id" element={<AlbumView />} />
-//           <Route path="/artist/:id" element={<ArtistView />} />
-//         </Routes>
-//       </Router>
-//     </div>
-//     /* <SearchContext.Provider
-//         value={{
-//           term: searchInput,
-//           handleSearch: handleSearch,
-//         }}
-//       >
-//         <SearchBar />
-//       </SearchContext.Provider>
-//       {message}
-//       <DataContext.Provider value={data}>
-//         <Gallery />
-//       </DataContext.Provider> */
-//   );
-// }
-
-// export default App;
 import { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Gallery from "./components/Gallery";
-import SearchBar from "./components/SearchBar";
+import SearchBar from "./components/Searchbar";
 import AlbumView from "./components/AlbumView";
 import ArtistView from "./components/ArtistView";
 import { Fragment } from "react/cjs/react.production.min";
