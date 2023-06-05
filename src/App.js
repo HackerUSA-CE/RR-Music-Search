e0a873abd6b0971e593359572f5fccd487f95960
Juan Mireles <119257668+Kal0yanX@users.noreply.github.com>
2023-06-05 14:03:25 -0400

    completed assignment

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import {useEffect, useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Gallery from './components/Gallery'
 import SearchBar from './components/SearchBar'
+import AlbumView from './components/AlbumView'
+import ArtistView from './components/ArtistView'
 import { DataContext } from './context/DataContext'
 
 function App() {
@@ -33,13 +36,24 @@ function App() {
 
 	return (
 		<div>
-			<SearchBar handleSearch = {handleSearch}/>
-			{message}
-			<DataContext.Provider value={data}>
-				<Gallery />
-			</DataContext.Provider>
+		{message}
+			<Router>
+				<Routes>
+					<Route path="/" element={
+						<>
+							<SearchBar handleSearch = {handleSearch}/>
+							<DataContext.Provider value={data}>
+							<Gallery data={data} />
+							</DataContext.Provider>
+						</>
+					} />
+					<Route path="/album/:id" element={<AlbumView />} />
+					<Route path="/artist/:id" element={<ArtistView />} />
+				</Routes>
+			</Router>
 		</div>
-  	);
+	)
+	;
 }
 
 export default App;
--- src/components/GalleryItem.js
+++ src/components/GalleryItem.js
@@ -1,4 +1,5 @@
 // GalleryItem.js
+import { Link } from 'react-router-dom'
 import { useState } from 'react' 
 
 function GalleryItem(props){
@@ -35,12 +36,22 @@ function GalleryItem(props){
         return (
             <div style={detailStyle}>
                 <h2>{props.item.trackName}</h2>
-                <h3>{props.item.collectionName}</h3>
+                <h3>
+                    <Link to={`/artist/${props.item.artistId}`}>
+                        {props.item.artistName}
+                    </Link>
+                </h3>
+                <h3>
+                    <Link to={`/album/${props.item.collectionId}`}>
+                        {props.item.collectionName}
+                    </Link>
+                </h3>
                 <h4>{props.item.primaryGenreName}</h4>
                 <h4>{props.item.releaseDate}</h4>
             </div>
         )
     }
+    
 
     return (
         <div onClick={() =>setView(!view)} style={{'display': 'inline-block'}}>
