5f385bffaaf77c85b7b5901b3aa3afe146895de1
aminatol <aminatoluwo@gmail.com>
2023-08-13 01:32:28 -0400

    artist and album components

---
+++ src/components/ArtistView.js
@@ -0,0 +1,20 @@
+import {useEffect, useState} from 'react'
+import {useParams} from 'react-router-dom'
+
+
+
+function ArtistView(){
+const {id}= useParams()
+    const [artistData, setArtistData]= useState ([])
+
+return(
+
+    <div>
+        <h2>The id passed was: {id}</h2>
+        <p> Artist Data Goes Here!</p>
+    </div>
+)
+}
+
+
+export default ArtistView
