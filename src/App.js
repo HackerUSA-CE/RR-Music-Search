3bf2e1f49e8d92e9bd6bad2fc53e43ea359e8b38
aminatol <aminatoluwo@gmail.com>
2023-08-14 21:21:52 -0400

    added context and BrowserRouter

--- src/App.js
+++ src/App.js
@@ -5,36 +5,30 @@ import SearchBar from "./components/SearchBar";
 import AlbumView from "./components/AlbumView";
 import ArtistView from "./components/ArtistView";
 import { Fragment } from "react/cjs/react.production.min";
+import { DataContext } from "./context/DataContext";
+
+
 
 function App() {
   let [search, setSearch] = useState("");
   let [message, setMessage] = useState("Search for Music!");
   let [data, setData] = useState([]);
 
-  const API_URL = "https://itunes.apple.com/search?term=";
+  // const API_URL = "https://itunes.apple.com/search?term=";
 
   useEffect(() => {
     if (search) {
+      document.title = `${search} Music`;
       const fetchData = async () => {
-        document.title = `${search} Music`;
-        console.log("before fetch")
-        // const response = await fetch(API_URL + search);
-        await fetch(API_URL + search, {
-          mode: "no-cors",
-        })
-          .then((res) => {
-            console.log(res);
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-         console.log("after fetch");
-        // const resData = await response.json();
-        // if (resData.results.length > 0) {
-        //   return setData(resData.results);
-        // } else {
-        //   return setMessage("Not Found");
-        // }
+        const response = await fetch(
+          `https://itunes.apple.com/search?term=${search}`
+        );
+        const resData = await response.json();
+        if (resData.results.length > 0) {
+          setData(resData.results);
+        } else {
+          setMessage("Not Found");
+        }
       };
       fetchData();
     }
@@ -42,7 +36,7 @@ function App() {
 
   const handleSearch = (e, term) => {
     e.preventDefault();
-    setSearch(term);
+    setSearch(search);
   };
 
   return (
@@ -55,7 +49,9 @@ function App() {
             element={
               <Fragment>
                 <SearchBar handleSearch={handleSearch} />
-                <Gallery data={data} />
+                <DataContext.Provider data={data}>
+                  <Gallery />
+                </DataContext.Provider>
               </Fragment>
             }
           />
--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,7 +1,11 @@
 import GalleryItem from "./GalleryItem";
+import { useContext } from "react";
+import { DataContext } from "../context/DataContext";
 
-function Gallery(props) {
-  const display = props.data.map((item, index) => {
+
+function Gallery() {
+  const data= useContext(DataContext)
+  const display = data.map((item, index) => {
     return <GalleryItem item={item} key={index} />;
   });
   return <div>{display}</div>;
