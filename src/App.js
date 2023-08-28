f65fab6772c124fe1a40472379929e5dcf69d5a1
Brandon <oallelsefailo@gmail.com>
2023-08-28 07:35:45 -0700

    Added with router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import { useEffect, useState } from "react";
-import Gallery from "./components/Gallery.js";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Gallery from "./components/Gallery";
 import SearchBar from "./components/SearchBar";
+import AlbumView from "./components/AlbumView";
+import ArtistView from "./components/ArtistView";
 
-const App = () => {
+function App() {
   let [search, setSearch] = useState("");
   let [message, setMessage] = useState("Search for Music!");
   let [data, setData] = useState([]);
@@ -12,13 +15,13 @@ const App = () => {
   useEffect(() => {
     if (search) {
       const fetchData = async () => {
-        document.title = `${search} music`;
+        document.title = `${search} Music`;
         const response = await fetch(API_URL + search);
         const resData = await response.json();
         if (resData.results.length > 0) {
           return setData(resData.results);
         } else {
-          return setMessage("Not Found.");
+          return setMessage("Not Found");
         }
       };
       fetchData();
@@ -32,11 +35,24 @@ const App = () => {
 
   return (
     <div>
-      <SearchBar handleSearch={handleSearch} />
       {message}
-      <Gallery data={data} />
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <SearchBar handleSearch={handleSearch} />
+                <Gallery data={data} />
+              </>
+            }
+          />
+          <Route path="/album/:id" element={<AlbumView />} />
+          <Route path="/artist/:id" element={<ArtistView />} />
+        </Routes>
+      </Router>
     </div>
   );
-};
+}
 
 export default App;
