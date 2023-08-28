bccbdccae5dfe558fa5fa43c1fe035e77ed12881
Brandon <oallelsefailo@gmail.com>
2023-08-28 07:31:57 -0700

    Hm

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import { useEffect, useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Gallery from "./components/Gallery";
+import Gallery from "./components/Gallery.js";
 import SearchBar from "./components/SearchBar";
-import AlbumView from "./components/AlbumView";
-import ArtistView from "./components/ArtistView";
-import SuspenseEx from "./components/Suspense";
-import { Fragment } from "react/cjs/react.production.min";
 
-function App() {
+const App = () => {
   let [search, setSearch] = useState("");
   let [message, setMessage] = useState("Search for Music!");
   let [data, setData] = useState([]);
@@ -17,13 +12,13 @@ function App() {
   useEffect(() => {
     if (search) {
       const fetchData = async () => {
-        document.title = `${search} Music`;
+        document.title = `${search} music`;
         const response = await fetch(API_URL + search);
         const resData = await response.json();
         if (resData.results.length > 0) {
           return setData(resData.results);
         } else {
-          return setMessage("Not Found");
+          return setMessage("Not Found.");
         }
       };
       fetchData();
@@ -37,25 +32,11 @@ function App() {
 
   return (
     <div>
+      <SearchBar handleSearch={handleSearch} />
       {message}
-      <Router>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <Fragment>
-                <SearchBar handleSearch={handleSearch} />
-                <Gallery data={data} />
-                <SuspenseEx />
-              </Fragment>
-            }
-          />
-          <Route path="/album/:id" element={<AlbumView />} />
-          <Route path="/artist/:id" element={<ArtistView />} />
-        </Routes>
-      </Router>
+      <Gallery data={data} />
     </div>
   );
-}
+};
 
 export default App;
